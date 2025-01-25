48aa3e4649e6ff025507eaa09e7c7e0190575c25
Abhishek Shende <abhishekshende17@gmail.com>
2025-01-25 10:49:10 +0530

    used created elastic ip in inbound rule

--- security.tf
+++ security.tf
@@ -20,3 +20,26 @@ resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
   cidr_ipv4         = "0.0.0.0/0"
   ip_protocol       = "-1" # semantically equivalent to all ports
 }
+
+resource "aws_eip" "lb" {
+  domain   = "vpc"
+}
+
+resource "aws_security_group" "from_eip" {
+  name        = "from_eip"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+}
+
+resource "aws_vpc_security_group_ingress_rule" "from_eip_ipv4" {
+  security_group_id = aws_security_group.from_eip.id
+  cidr_ipv4         = "${aws_eip.lb.public_ip}/32"
+  from_port         = 443
+  ip_protocol       = "tcp"
+  to_port           = 443
+}
+
+resource "aws_vpc_security_group_egress_rule" "from_eip_traffic_ipv4" {
+  security_group_id = aws_security_group.from_eip.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
